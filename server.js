1dad501cfd15fe4f5dd339232d193f929a89549b
Kazkar <iglu963@gmail.com>
2025-07-15 02:43:37 +0300

    refactor: use express built-in json

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const axios = require('axios');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 require('dotenv').config();
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, 'cimeika-api.yaml'));
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
