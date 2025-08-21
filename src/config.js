a46e66e97231843217c9699fc1e314a602b57bf6
Kazkar <iglu963@gmail.com>
2025-08-21 12:51:18 +0300

    Add cache TTL to config and sync client/server

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const schema = Joi.object({
   HUGGINGFACE_TOKEN: Joi.string(),
   DEFAULT_CITY: Joi.string().default('London'),
-  DEFAULT_SIGN: Joi.string().default('aries')
+  DEFAULT_SIGN: Joi.string().default('aries'),
+  CACHE_TTL_MS: Joi.number().integer().min(0).default(5 * 60 * 1000)
 }).unknown();
 
 const { value: env, error } = schema.validate(process.env, { abortEarly: false });
@@ -12,5 +13,6 @@ module.exports = {
   hfToken: env.HUGGINGFACE_TOKEN,
   defaultCity: env.DEFAULT_CITY,
   defaultSign: env.DEFAULT_SIGN,
+  cacheTtlMs: env.CACHE_TTL_MS,
   error
 };
